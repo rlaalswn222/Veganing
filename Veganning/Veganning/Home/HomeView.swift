a44b0c4383c25640f507bddb1edc28ee42c54fdd
minsolkim <123alsthf@naver.com>
2024-08-17 21:54:30 +0900

    Feat[#17] homeview 카드뷰

--- Veganning/Veganning/Home/HomeView.swift
+++ Veganning/Veganning/Home/HomeView.swift
@@ -2,20 +2,47 @@
 //  HomeView.swift
 //  Veganning
 //
-//  Created by 김민솔 on 8/11/24.
+//  Created by 김민솔 on 8/17/24.
 //
-
 import SwiftUI
 
-struct HomeView: View {
-    var body: some View {
-        Text("Hello, World!")
-        Image("food_image")
-            .resizable()
-            .frame(width: 340,height: 700)
-    }
+struct Recipe: Identifiable {
+    let id = UUID()
+    let title: String
 }
 
-#Preview {
-    HomeView()
+struct HomeView: View {
+    let recipes = [
+        Recipe(title: "Other POKE"),
+        Recipe(title: "Green POKE"),
+        Recipe(title: "Another Recipe")
+    ]
+    
+    var body: some View {
+        ScrollView {
+            ZStack {
+                Image("homeBack")
+                    .resizable()
+                    .frame(maxWidth: .infinity, maxHeight: 630)
+                    .clipped()
+                VStack(alignment: .leading) {
+                    HomeTagsView()
+                        .padding(.top,50)
+                    CarouselView(items: recipes)
+                        .padding(.top,-20)
+                    Text("TODAY| 240715 D+37")
+                        .foregroundColor(.white)
+                        .font(.system(size: 20,weight: .bold))
+                    Text("have a good vaganing day")
+                        .foregroundColor(.white)
+                        .font(.system(size: 12))
+                }
+                
+            }
+            Text("HOT 비건 식당 >")
+                .foregroundColor(.black)
+                .font(.system(size: 20))
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
 }
