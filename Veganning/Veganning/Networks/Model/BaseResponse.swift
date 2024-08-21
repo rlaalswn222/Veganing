f1c1af40092a554378f649b6ae48f2add4d96f1d
minsolkim <123alsthf@naver.com>
2024-08-22 04:08:31 +0900

    Revert "main commit 취소 소은님"
    This reverts commit 9330591d7b6f97823b8a55b086a7cbeeb13b1ec7.

--- Veganning/Veganning/Networks/Model/BaseResponse.swift
+++ Veganning/Veganning/Networks/Model/BaseResponse.swift
@@ -1,15 +1,9 @@
-//
-//  BaseResponse.swift
-//  Veganning
-//
-//  Created by 김민솔 on 8/21/24.
-//
-
 import Foundation
 
 struct BaseResponse<T: Codable>: Codable {
     let isSuccess: Bool
-    let code: Int
-    let data: T?
+    let code: String
     let message: String
+    let data: T?
 }
+
